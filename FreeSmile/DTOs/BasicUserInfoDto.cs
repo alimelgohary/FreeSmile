def90a2ee9741f0fe88f032a2fc63f8d3d5f6dc2
alimelgohary <ali_algohary2001@outlook.com>
2023-04-29 21:08:59 +0300

    forgot its DTOs

---
+++ FreeSmile/DTOs/BasicUserInfoDto.cs
@@ -0,0 +1,11 @@
+﻿namespace DTOs
+{
+    public class BasicUserInfoDto
+    {
+        public int UserId { get; set; }
+        public string FullName { get; set; } = null!;
+        public string Username { get; set; } = null!;
+        public string? AcademicDegree { get; set; } = null;
+        public byte[]? ProfilePicture { get; set; }
+    }
+}
--- FreeSmile/DTOs/RecentMessagesDto.cs
+++ FreeSmile/DTOs/RecentMessagesDto.cs
@@ -1,6 +1,13 @@
-﻿namespace FreeSmile.DTOs
+﻿using DTOs;
+
+namespace FreeSmile.DTOs
 {
-    public class RecentMessagesDto
+    public class RecentMessagesDto : BasicUserInfoDto
     {
+        public string LastMessage { get; set; } = null!;
+        public string LastMessageTime { get; set; } = null!;
+        public bool IsSender { get; set; }
+        public bool Seen { get; set; }
+        public bool IsAvailable { get; set; }
     }
 }
